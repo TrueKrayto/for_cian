bae676a7dbe6708d737153e8c0b88b9f80b0677e
Krayto <159171793+TrueKrayto@users.noreply.github.com>
2024-06-01 15:23:01 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,30 @@
+#!/usr/bin/env python
 
+from groq import Groq
+
+client = Groq(
+    api_key="put in your api code in here between the quotes"
+)
+
+def main():
+    print("")
+    dict = {"role": "user","content": ""}
+    continue_chat = True
+    while continue_chat== True:
+        question = input("> ")
+        if question.lower() == "quit":
+            continue_chat = False
+            dict["content"] = "Say goodby to the user"
+            chat_completion =client.chat.completions.create(messages=[dict], model="llama3-8b-8192",)
+            print("")
+            print(chat_completion.choices[0].message.content)
+            print("")
+                  
+        else:
+            dict["content"] = question
+            chat_completion =client.chat.completions.create(messages=[dict], model="llama3-8b-8192",)
+            print("")
+            print(chat_completion.choices[0].message.content)
+            print("")
+    
+main()
